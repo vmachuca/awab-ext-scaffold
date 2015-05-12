928f5110f5f3a25c6b35c9495a3e6a7042be96c2
Laurindo Ferreira Lucio Junior <laurindofljunior@gmail.com>
2015-05-12 16:03:46 -0300

    Acrecenta task Copy ao grunt config.

--- generator-img-awab-ext/app/templates/gruntfile.js
+++ generator-img-awab-ext/app/templates/gruntfile.js
@@ -10,10 +10,26 @@ module.exports = function (grunt) {
     var appDir = '<%= apppath %>';
     var stemappDir = '<%= steamapp %>';
     grunt.initConfig({
+        copy: {
+            main: {},
+            js: {
+              expand: true,
+              cwd: 'src/widgets',
+              src: '**/*.js',
+              dest: 'src/widgets',
+              filter: function(filepath) {
+                var path = require('path');
+                var destFinal = filepath.split(path.sep).slice(0,filepath.split(path.sep).length -1).join(path.sep);
+                grunt.log.writeln(("Criando o diretorio '"+destFinal+"'")["cyan"].bold);
+                grunt.file.mkdir(destFinal.concat(path.sep).concat("specs"));
+                return false;
+              }
+            }
+        },
         watch: {
             main: {
                 files: ['src/**'],
-                tasks: ['sync'],
+                tasks: ['copy:js','sync'],
                 options: { spawn: false }
             }
         },
@@ -51,9 +67,11 @@ module.exports = function (grunt) {
             }
         }
     });
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-sync');
     grunt.registerTask('default', [
+        'copy:js',
         'sync',
         'watch'
     ]);
